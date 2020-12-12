58e1930fa92936b6e214958f3947ee4b0abd1b57
rafael-santiago <voidbrainvoid@tutanota.com>
2020-12-12 19:06:42 -0200

    Versioning 'aegis.c'

---
+++ src/aegis.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020, Rafael Santiago
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#include <stdlib.h>
+#include <unistd.h>
+
+int aegis_default_on_debugger(void *args) {
+    exit(1);
+}
+
