fbdaffbdaa08d4212e228e948cef274334454d7a
rafael-santiago <voidbrainvoid@tutanota.com>
2021-11-01 21:42:05 -0300

    Implement a more general detection strategy against Procmon
     But by now it follows disabled.

--- src/native/windows/aegis_native.c
+++ src/native/windows/aegis_native.c
@@ -6,11 +6,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 #include <aegis.h>
+#include <windows.h>
 #include <debugapi.h>
 #include <intrin.h>
 #include <winternl.h>
 #include <processthreadsapi.h>
 #include <synchapi.h>
+#include <fltuser.h>
 
 #if !defined(CGO)
 struct aegis_gorgon_exec_ctx {
@@ -26,8 +28,70 @@ static struct aegis_gorgon_exec_ctx g_aegis_gorgon = { 0, NULL, NULL, NULL, NULL
 static DWORD WINAPI aegis_gorgon_routine(LPVOID args);
 #endif // !defined(CGO)
 
+#if 0
+
+static NTSTATUS is_procmon_sc_registered(const wchar_t *service_name, const size_t service_name_size);
+
+static HRESULT tryopen_procmon_sc(const wchar_t *service_name);
+
+BOOL do_is_procmon_present(const wchar_t *service_name, const size_t service_name_size);
+
+BOOL is_procmon_present(void);
+
+static NTSTATUS is_procmon_sc_registered(const wchar_t *service_name, const size_t service_name_size) {
+    HKEY hk;
+    wchar_t w_subkey[1024] = L"";
+    wcsncpy_s(w_subkey, sizeof(w_subkey) - 2, L"System\\CurrentControlSet\\Services\\",
+    wcslen(L"System\\CurrentControlSet\\Services\\"));
+    wcsncat_s(w_subkey, sizeof(w_subkey) - 2, service_name, service_name_size);
+    NTSTATUS retval = RegOpenKeyExW(HKEY_LOCAL_MACHINE, w_subkey, 0, KEY_QUERY_VALUE, &hk);
+    if (retval == ERROR_SUCCESS) {
+        RegCloseKey(hk);
+    }
+    return retval;
+}
+
+static HRESULT tryopen_procmon_sc(const wchar_t *service_name) {
+    HFILTER filter;
+    HRESULT res = FilterCreate(service_name, &filter);
+    if (res == S_OK) {
+        FilterClose(filter);
+    }
+    return res;
+}
+
+BOOL do_is_procmon_present(const wchar_t *service_name, const size_t service_name_size) {
+    HRESULT res = tryopen_procmon_sc(service_name);
+    switch (res) {
+        case S_OK:
+            return TRUE;
+
+        case E_ACCESSDENIED: // INFO(Rafael): In dubio pro reo? No here! ;)
+            return (is_procmon_sc_registered(service_name, service_name_size) == ERROR_SUCCESS);
+    }
+    return FALSE;
+}
+
+BOOL is_procmon_present(void) {
+    static const wchar_t *procmon_scs[] = {
+        L"PROCMON24",
+        L"PROCMON23",
+    };
+    static const size_t procmon_scs_nr = sizeof(procmon_scs) / sizeof(procmon_scs[0]);
+    const wchar_t **service = &procmon_scs[0];
+    const wchar_t **service_end = service + procmon_scs_nr;
+    BOOL is = FALSE;
+    while (!is && service != service_end) {
+        is = do_is_procmon_present(*service, wcslen(*service));
+        service++;
+    }
+    return is;
+}
+
+#endif
+
 int aegis_has_debugger(void) {
-    int has = (IsDebuggerPresent() != FALSE);
+    int has = (IsDebuggerPresent() /*|| is_procmon_present()*/);
     PPEB peb = NULL;
     DWORD nt_global_flag = 0;
     if (!has) {
